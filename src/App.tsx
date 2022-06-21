6d80f4ece18514a6c2023c9108bc98c579b27b31
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2022-06-21 16:38:15 -0400

    Added initial header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import hero from './assets/mobile/image-hero.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={hero} className="hero-image" alt="people looking at painting" />
       </header>
+      <main className="main-content">
+        <h1 className="main-title">Modern <br />Art Gallery</h1>
+        <p>The arts in the collection of the Modern Art Gallery all started 
+            from a spark of inspiration. Will these pieces inspire you? Visit 
+            us and find out.
+        </p>
+        <div className="btn-container">
+          <a href="#" className="btn">our location</a>
+        </div>
+      </main>
+      
     </div>
   );
 }
